a0d1427bff8d95845934b654e40e6c90c713debb
Michael Neu <m.neu.1983@live.com>
2020-11-26 20:09:36 +0100

    init repo

---
+++ NextNearest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TravelingSalesman
+{
+
+    public record Position 
+    {
+        public int PositionNumber { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public bool IsFilled { get; set; }
+
+        public Position(int positionNumber, double x, double y, bool isFilled) => (PositionNumber, X, Y, IsFilled) = (positionNumber, x, y, isFilled);
+    }
+
+    public static class NextNearest
+    {
+
+        public static (List<Position> travelList, double distance) GetFastestWayToGetDrunk((double x, double y, bool isFilled)[] positions)
+        {
+            Position currentPosition = new Position(0, positions[0].x, positions[0].y, false);
+            List<Position> positionsToTravel  = new();
+            List<Position> getDrunkFastWayToTravel = new();
+            double travelledDistance = 0;
+
+            for (int i = 1; i < positions.Length; i++)
+            {
+                positionsToTravel.Add(new Position(i, positions[i].x, positions[i].y, positions[i].isFilled));
+            }
+
+
+            while (positionsToTravel.Any())
+            {
+                var result = GetNextFastestReachablePosition(currentPosition, positionsToTravel);
+                currentPosition = result.nextPosition;
+                travelledDistance += result.wayToTravel;
+                positionsToTravel.Remove(currentPosition);
+                getDrunkFastWayToTravel.Add(currentPosition);
+            }
+
+            return (getDrunkFastWayToTravel, Math.Sqrt(travelledDistance)); 
+        }
+
+        private static (Position nextPosition, double wayToTravel) GetNextFastestReachablePosition(Position currentPosition, List<Position> positionsToTravel)
+        {
+            var shortestWay = double.MaxValue;
+            Position nextPosition = null;
+            foreach(var position in positionsToTravel)
+            {
+                var distance = (currentPosition.X - position.X) * (currentPosition.X - position.X) + (currentPosition.Y - position.Y) * (currentPosition.Y - position.Y);
+                if (distance < shortestWay)
+                {
+                    shortestWay = distance;
+                    nextPosition = position;
+                }
+            }
+
+            return (nextPosition, shortestWay);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+
+namespace TravelingSalesman
+{
+    class Program
+    {
+        private static double PanelWidth = 1000;
+        private static double PanelHeight = 1000;
+        private static int GlassCount = 12;
+
+        static void Main(string[] args)
+        {
+            var startTime = DateTime.Now;
+            Console.WriteLine("Start: "+ startTime);
+            var glasses = GetTwelveGlasses();
+            var (minLength, minPath) = PathSearch(glasses);
+            var endTime = DateTime.Now;
+            Console.WriteLine("End: " + endTime);
+            var timeSpan = endTime - startTime;
+            Console.WriteLine("Time: " + timeSpan);
+            Console.WriteLine("MinLength: " + minLength);
+
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++");
+            
+            startTime = DateTime.Now;
+            Console.WriteLine("Start: " + startTime);
+            var nextNearestFastestWay = NextNearest.GetFastestWayToGetDrunk(glasses);
+            endTime = DateTime.Now;
+            Console.WriteLine("End: " + endTime);
+            timeSpan = endTime - startTime;
+            Console.WriteLine("Time: " + timeSpan);
+            Console.WriteLine("MinLength: " + nextNearestFastestWay.distance);
+
+            Console.ReadKey();
+        }
+
+        private static (double, int[]) PathSearch((double, double, bool)[] glasses)
+        {
+            var currentPath = new Stack<int>();
+            var minPath = new int[0];
+            var minLength = double.MaxValue;
+
+
+            PathSearch(0, 0, glasses, currentPath, ref minLength, ref minPath);
+            minLength = Math.Sqrt(minLength);
+
+            return (minLength, minPath);
+        }
+
+        private static void PathSearch(double currentLength, int fromGlass, (double, double, bool)[] glasses, Stack<int> currentStack, ref double minLength, ref int[] minPath)
+        {
+            for (int i = 1; i <= GlassCount; i++)
+            {
+                if (!glasses[i].Item3)
+                {
+                    var x1 = glasses[fromGlass].Item1;
+                    var y1 = glasses[fromGlass].Item2;
+                    var x2 = glasses[i].Item1;
+                    var y2 = glasses[i].Item2;
+                    var pathLength = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + currentLength;
+
+                    currentStack.Push(i);
+                    glasses[i].Item3 = true;
+                    if (currentStack.Count < GlassCount)
+                    {
+                        PathSearch(pathLength, i, glasses, currentStack, ref minLength, ref minPath);
+                    }
+                    else
+                    {
+                        if (pathLength < minLength)
+                        {
+                            minLength = pathLength;
+                            minPath = currentStack.ToArray();
+                        }
+                    }
+                    glasses[i].Item3 = false;
+                    currentStack.Pop();
+                }
+            }
+        }
+
+        private static (double, double, bool)[] GetTwelveGlasses()
+        {
+            var xStart = 0;
+            var yStart = 0;
+            var startPoint = new (double, double, bool)[] {(xStart, yStart, true)};
+
+            var rand = new Random();
+            var sequence = startPoint.Concat(Enumerable.Range(0, GlassCount).Select(i => (rand.NextDouble() * PanelWidth, rand.NextDouble() * PanelHeight, false))).ToArray();
+            return sequence;
+        }
+    }
+}
